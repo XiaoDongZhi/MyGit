749cf569663eaaac2d8a3ea00a2811aa34431dcc
WesTreLLi <517341003@qq.com>
2015-02-09 09:24:03 -0800

    commite

---
+++ CameraEnhance/CameraEnhance/src/com/example/cameraenhance/GPSListener.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.example.cameraenhance;
+
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class GPSListener extends Service implements OnClickListener
+{
+	private LocationManager lm;
+
+	private final Context mContext;
+
+	public GPSListener(Context context)
+	{
+		this.mContext = context;
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+		case R.id.button_start_gps:
+			startGps();
+			break;
+		case R.id.target_settings:
+			startTargetSetting();
+			break;
+
+		}
+
+	}
+
+	private void startTargetSetting()
+	{
+		Intent intent = new Intent(mContext, TargetSetting.class);
+		mContext.startActivity(intent);
+	}
+
+	private void startGps()
+	{
+		lm = (LocationManager) mContext
+				.getSystemService(Context.LOCATION_SERVICE);
+
+		// 判断GPS是否正常启动
+		if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+		{
+			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			mContext.startActivity(intent);
+			return;
+		} else
+		{
+			Intent intent = new Intent(mContext, GpsInfo.class);
+			mContext.startActivity(intent);
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
